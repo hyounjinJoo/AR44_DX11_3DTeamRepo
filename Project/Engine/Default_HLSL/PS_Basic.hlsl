6749b3929f2ba550c7e6911751f22ab3fa3efe99
HWNOTE <ekdrn1@naver.com>
2023-07-26 16:29:02 +0900

    HLSL 컴파일 에러뜨는거 제거

--- Project/Engine/Default_HLSL/PS_Basic.hlsl
+++ Project/Engine/Default_HLSL/PS_Basic.hlsl
@@ -48,7 +48,7 @@ float4 main(VSOut In) : SV_Target
 	if (usedNormal == 1)
 	{
 	// 물체의 표면에 적용될 탄젠트 공간 기준 방향벡터를 가져온다.
-			vNormal = normalTexture.Sample(anisotropicSampler, In.UV);
+			vNormal = normalTexture.Sample(anisotropicSampler, In.UV).xyz;
 
 	// 0~1값을 -1~1의 값으로 변환
 			vNormal = (vNormal * 2.0f) - 1.0f;
@@ -65,7 +65,7 @@ float4 main(VSOut In) : SV_Target
 
 
 	LightColor lightColor = (LightColor)0.0f;
-	for (int i = 0; i < numberOfLight; i++)
+	for (uint i = 0; i < numberOfLight; i++)
 	{
 		CalculateLight3D(In.ViewPos, vNormal, i, lightColor);
 	}
