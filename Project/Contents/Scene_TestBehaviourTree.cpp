48056df060140457eb092679310ab6fa5badc465
Hyoun Jin Joo ( 주현진 ) <hyounjin.dev@gmail.com>
2023-08-26 15:44:56 +0900

    Scene_TestBehaviourTree 부모 클래스 함수 호출 추가
    [Modify]
    Scene_TestBehaviourTree.cpp : 각 함수가 부모 클래스의 함수를 호출하도록 수정

--- Project/Contents/Scene_TestBehaviourTree.cpp
+++ Project/Contents/Scene_TestBehaviourTree.cpp
@@ -5,6 +5,7 @@
 namespace mh
 {
 	Scene_TestBehaviourTree::Scene_TestBehaviourTree()
+		: IScene()
 	{
 	}
 
@@ -14,29 +15,36 @@ namespace mh
 
 	void Scene_TestBehaviourTree::OnEnter()
 	{
+		IScene::OnEnter();
 	}
 
 	void Scene_TestBehaviourTree::Init()
 	{
+		IScene::Init();
 	}
 
 	void Scene_TestBehaviourTree::Update()
 	{
+		IScene::Update();
 	}
 
 	void Scene_TestBehaviourTree::FixedUpdate()
 	{
+		IScene::FixedUpdate();
 	}
 
 	void Scene_TestBehaviourTree::Render()
 	{
+		IScene::Render();
 	}
 
 	void Scene_TestBehaviourTree::Destroy()
 	{
+		IScene::Destroy();
 	}
 
 	void Scene_TestBehaviourTree::OnExit()
 	{
+		IScene::OnExit();
 	}
 }
