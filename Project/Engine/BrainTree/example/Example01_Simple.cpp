7dcbf65e932f769e54786263f167976f0e089464
Hyoun Jin Joo ( 주현진 ) <hyounjin.dev@gmail.com>
2023-08-26 15:28:26 +0900

    BrainTree Example에 네임 스페이스 적용
    [Modify]
    Example01_Simple.cpp : 네임 스페이스 BrainTree 적용

--- Project/Engine/BrainTree/example/Example01_Simple.cpp
+++ Project/Engine/BrainTree/example/Example01_Simple.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include "../BrainTree.h"
 
-namespace bt = BrainTree;
-
-class SayHello : public bt::Node
+namespace BrainTree
 {
-    Status update() override
+    class SayHello : public Node
     {
-        std::cout << "Hello, World!" << std::endl;
-        return Node::Status::Success;
+        Status update() override
+        {
+            std::cout << "Hello, World!" << std::endl;
+            return Node::Status::Success;
+        }
+    };
+
+    int Example()
+    {
+        BehaviorTree tree;
+
+        auto repeater = std::make_shared<Repeater>(5);
+        repeater->setChild(std::make_shared<SayHello>());
+        tree.setRoot(repeater);
+
+        // simulate 5 frames
+        for (int i = 0; i < 5; i++)
+            tree.update();
+
+        return 0;
     }
-};
-
-int Example()
-{
-    bt::BehaviorTree tree;
-
-    auto repeater = std::make_shared<bt::Repeater>(5);
-    repeater->setChild(std::make_shared<SayHello>());
-    tree.setRoot(repeater);
-
-    // simulate 5 frames
-    for (int i = 0; i < 5; i++)
-        tree.update();
-
-    return 0;
 }
