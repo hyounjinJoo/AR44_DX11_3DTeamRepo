0d0d909403f86f35444ac4d5dc335e5e2e407189
Hyoun Jin Joo ( 주현진 ) <hyounjin.dev@gmail.com>
2023-09-04 12:29:42 +0900

    Test Scene 생성

---
+++ Project/Contents/Scene_TestSkyBox.cpp
@@ -0,0 +1,40 @@
+#include "Scene_TestSkyBox.h"
+#include <Engine\EventMgr.h>
+#include <Engine\Com_Renderer_Mesh.h>
+#include <Engine\ResMgr.h>
+
+namespace mh
+{
+	Scene_TestSkyBox::Scene_TestSkyBox()
+	{
+
+		{
+		}
+	}
+
+	Scene_TestSkyBox::~Scene_TestSkyBox()
+	{
+
+	}
+	void Scene_TestSkyBox::OnEnter()
+	{
+	}
+	void Scene_TestSkyBox::Init()
+	{
+	}
+	void Scene_TestSkyBox::Update()
+	{
+	}
+	void Scene_TestSkyBox::FixedUpdate()
+	{
+	}
+	void Scene_TestSkyBox::Render()
+	{
+	}
+	void Scene_TestSkyBox::Destroy()
+	{
+	}
+	void Scene_TestSkyBox::OnExit()
+	{
+	}
+}
